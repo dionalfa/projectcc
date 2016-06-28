37d32c7de67176dbc4685379651a49f381b6a826
dionisius <dion.alfaputra1@gmail.com>
2016-06-28 14:47:52 +0700

    fix if else condition add project member

--- chaincode.go
+++ chaincode.go
@@ -428,7 +428,7 @@ func (t *SimpleChaincode) add_project_member(stub *shim.ChaincodeStub, args []st
 
 	for i:=1; i < len(args); i++ {
 
-		if(len(project.Members) == 0){
+		if len(project.Members) == 0 {
 			member = Member{}
 			member.MemberID = args[i]
 			member.MemberName =  args[i+1]
